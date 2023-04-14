d50d2e6d63cede6fb1cb33042c88d802b94ad763
Daniel <dm@spyglassventures.ch>
2023-04-14 16:14:51 +0200

    add css

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,11 @@
 const Search = () => {
     return (
         <header>
-            <h2 className="header__title">Search it. Explore it. Buy it.</h2>
+            <h2 className="header__title">Tokenize it. Buy it. Drive it.</h2>
             <input
                 type="text"
                 className="header__search"
-                placeholder="Enter an address, neighborhood, city, or ZIP code"
+                placeholder="Enter car brand or type."
             />
         </header>
     );
